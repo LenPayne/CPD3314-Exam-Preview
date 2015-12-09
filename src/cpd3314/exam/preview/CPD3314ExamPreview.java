4643af82eeaf5a9d56345b6dbc1043cb9e7ceb37
Len Payne <len.payne@lambtoncollege.ca>
2015-12-09 08:49:09 -0500

    Clarified expected toString output

--- src/cpd3314/exam/preview/CPD3314ExamPreview.java
+++ src/cpd3314/exam/preview/CPD3314ExamPreview.java
@@ -23,8 +23,7 @@ package cpd3314.exam.preview;
  * Information about IMO Numbers can be found at https://en.wikipedia.org/wiki/IMO_number
  */
 
-/*
- * Design a Ship class that has the following members:
+/* Design a Ship class that has the following members:
  * - A field for the name of the ship (name:String)
  * - A field for the id of the ship (id:String)
  * - A field for the year that the ship was built (year:String)
@@ -38,7 +37,7 @@ package cpd3314.exam.preview;
  *   "IMO 9074729": (9×7) + (0×6) + (7×5) + (4×4) + (7×3) + (2×2) = 139. If the
  *   check fails, the setId() method should throw an Exception.
  * - A toString method that displays the ship's name, id and the year it was 
- *   built.
+ *   built. eg- "SS Minnow - IMO 9174721 - 1964"
  *
  * Design a CruiseShip class that extends the Ship class. Teh CruiseShip class 
  * should have the following additional members:
@@ -46,7 +45,8 @@ package cpd3314.exam.preview;
  * - A no-arg constructor and appropriate accessors and mutators
  * - A toString method that overrides the toString method in the base class. The 
  *   CruiseShip class's toString method should display only the ship's name, id,
- *   and  the maximum number of passengers.
+ *   year it was built, and the maximum number of passengers.
+ *   eg- "RMS Titanic, IMO 9074729, 1912, 2435 passengers"
  *
  * Design a CargoShip class that extends the Ship class. The CargoShip class 
  * should have the following members:
@@ -54,15 +54,15 @@ package cpd3314.exam.preview;
  * - A no-arg constructor and appropriate accessors and mutators
  * - A toString method that overrides the toString method in the base class. The 
  *   CargoShip class's toString method should dispaly only the ship's name, id,
- *   and the ship's cargo capacity.
+ *   year it was built, and the ship's cargo capacity.
+ *   eg- "SS Edmund Fitzgerald, IMO 8074728, 1958, 25400 tons"
  *
  * Demonstrate the classes in the main method with an array of Ship objects and
  * iterate over them, calling their toString method.
  */
-
 /**
  *
- * @author Len Payne <len.payne@lambtoncollege.ca>
+ * @author <ENTER YOUR NAME HERE>
  */
 public class CPD3314ExamPreview {
 
@@ -70,7 +70,9 @@ public class CPD3314ExamPreview {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* TODO: Demonstrate the classes in the main method with an array of 
+         * Ship objects and iterate over them, calling their toString method.
+         */
     }
-    
+
 }
